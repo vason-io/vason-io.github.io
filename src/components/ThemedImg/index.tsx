8cdc695abe438d468b8cda5ee9b036859303dec1
Arslan Charyyev <88978221+arslan-charyyev@users.noreply.github.com>
2023-12-30 15:34:07 +0500

    Refactored components to TS + Module CSS

--- src/components/ThemedImg/index.tsx
+++ src/components/ThemedImg/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ThemedImage from '@docusaurus/theme-classic/lib/theme/ThemedImage';
 
-export const ThemedImg = ({ alt = undefined, name }) => (
+export const ThemedImg = ({ name, alt }: { name: string; alt?: string }) => (
   <ThemedImage
     alt={alt ?? name}
     sources={{
