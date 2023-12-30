3062d37418c8fca124a4e6967f1d33f801b90924
ilya zuev <ilvizuev@gmail.com>
2023-12-30 10:52:51 +0100

    dark mode problem

--- src/components/ThemedImg/index.js
+++ src/components/ThemedImg/index.js
@@ -3,25 +3,33 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import { useColorMode } from '@docusaurus/theme-common';
 
 if (ExecutionEnvironment.canUseDOM) {
-  document.onreadystatechange = function (e) {
-    console.log('onreadystatechange');
-    const theme = document.body.parentElement.dataset.theme;
-    for (const img of document.getElementsByTagName('IMG')) {
-      img.src =
-        theme === 'dark'
-          ? img.src.replace('_light.', '_dark.')
-          : img.src.replace('_dark.', '_light.');
-    }
-  };
+  document.onreadystatechange = function (e) {};
   // We also need to setCodeRevealTriggers when the page first loads; otherwise,
   // after reloading the page, these triggers will not be set until the user
   // navigates somewhere.
   window.addEventListener('load', () => {
-    console.log('load');
+    const theme = document.body.parentElement.dataset.theme;
+    setTimeout(() => {
+      let imgs = document.getElementsByTagName('IMG');
+      console.log(imgs.length);
+      for (const img of imgs) {
+        img.src =
+          theme === 'dark'
+            ? img.src.replace('_light.', '_dark.')
+            : img.src.replace('_dark.', '_light.');
+        img.style.visibility = 'visible';
+      }
+    }, 200);
   });
 }
 
 export default function ThemedImg({ alt, name }) {
   const { colorMode } = useColorMode();
-  return <img src={`/img/${name}_${colorMode}.png`} alt={alt ?? name} />;
+  return (
+    <img
+      src={`/img/${name}_${colorMode}.png`}
+      alt={alt ?? name}
+      style={{ visibility: 'hidden' }}
+    />
+  );
 }
