57122ddd6978d3959bac2286ac44580a6d2fb533
Arslan Charyyev <88978221+arslan-charyyev@users.noreply.github.com>
2023-12-30 14:53:59 +0500

    Fixed ThemedImg

--- src/components/ThemedImg/index.js
+++ src/components/ThemedImg/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
-import { useColorMode } from '@docusaurus/theme-common';
+import ThemedImage from '@docusaurus/theme-classic/lib/theme/ThemedImage';
 
-if (ExecutionEnvironment.canUseDOM) {
-  // We also need to setCodeRevealTriggers when the page first loads; otherwise,
-  // after reloading the page, these triggers will not be set until the user
-  // navigates somewhere.
-  window.addEventListener('load', () => {
-    const theme = document.body.parentElement.dataset.theme;
-    setTimeout(() => {
-      let imgs = document.getElementsByTagName('IMG');
-      console.log(imgs.length);
-      for (const img of imgs) {
-        img.src =
-          theme === 'dark'
-            ? img.src.replace('_light.', '_dark.')
-            : img.src.replace('_dark.', '_light.'); //img.style.visibility = 'visible';
-      }
-    }, 100);
-  });
-}
-
-export default function ThemedImg({ alt, name }) {
-  const { colorMode } = useColorMode(); // style={{ visibility: 'hidden' }}
-  return <img src={`/img/${name}_${colorMode}.png`} alt={alt ?? name} />;
-}
+export default /*const ThemedImg = */ ({ alt = undefined, name }) => (
+  <ThemedImage
+    alt={alt ?? name}
+    sources={{
+      light: `/img/${name}_light.png`,
+      dark: `/img/${name}_dark.png`,
+    }}
+  />
+);
