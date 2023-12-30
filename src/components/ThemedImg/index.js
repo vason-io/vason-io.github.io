354dd8ce584e8d391c5a4bd78f7b5f040df20faa
ilya zuev <ilvizuev@gmail.com>
2023-12-30 01:01:38 +0100

    dark mode problem

--- src/components/ThemedImg/index.js
+++ src/components/ThemedImg/index.js
@@ -2,17 +2,24 @@ import React from 'react';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import { useColorMode } from '@docusaurus/theme-common';
 
+if (ExecutionEnvironment.canUseDOM) {
+  // We also need to setCodeRevealTriggers when the page first loads; otherwise,
+  // after reloading the page, these triggers will not be set until the user
+  // navigates somewhere.
+  window.addEventListener('load', () => {
+    setTimeout(() => {
+      const theme = document.body.parentElement.dataset.theme;
+      for (const img of document.getElementsByTagName('IMG')) {
+        img.src =
+          theme === 'dark'
+            ? img.src.replace('_light.', '_dark.')
+            : img.src.replace('_dark.', '_light.');
+      }
+    }, 100);
+  });
+}
+
 export default function ThemedImg({ alt, name }) {
   const { colorMode } = useColorMode();
-
-  return (
-    <img
-      src={`/img/${name}_${
-        ExecutionEnvironment.canUseDOM
-          ? localStorage.getItem('theme') // document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark'
-          : colorMode
-      }.png`}
-      alt={alt ?? name}
-    />
-  );
+  return <img src={`/img/${name}_${colorMode}.png`} alt={alt ?? name} />;
 }
